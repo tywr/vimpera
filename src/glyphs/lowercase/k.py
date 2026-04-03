453f9e7367e4796d40d1c60835cd02e24bde0007
tywr <tanguy.viv@gmail.com>
2026-04-03 23:18:01 +0200

    fix: k geometry and angles

--- src/glyphs/lowercase/k.py
+++ src/glyphs/lowercase/k.py
@@ -1,5 +1,4 @@
-from math import atan, cos, sin
-from config import FontConfig as fc
+from math import atan, cos, sin, sqrt, asin, atan2
 from glyph import Glyph
 from shapes.rect import draw_rect
 from shapes.polygon import draw_polygon
@@ -8,67 +7,61 @@ from shapes.polygon import draw_polygon
 class LowercaseKGlyph(Glyph):
     name = "lowercase_k"
     unicode = "0x6B"
+    offset = 10
+    bowl_width = 320
+    neck_len = 100
+    width_ratio = 1
+    neck_ratio = 0.35
 
-    def draw(
-        self,
-        pen,
-        stroke: int,
-    ):
-        offset = 10
-        bowl_width = 320
-        neck_len = 100
-        width = 380
+    def draw(self, pen, dc):
+        b = dc.body_bounds(offset=self.offset, width_ratio=self.width_ratio)
+        x_neck = b.x1 + self.neck_ratio * b.width
 
-        x1 = fc.width / 2 - bowl_width / 2 - stroke / 2 + offset
-        y1 = 0
-        x2 = x1 + width + stroke / 2
-        y2 = fc.x_height
+        # Angle of each diagonal branch
+        w = b.x2 - x_neck
+        h = dc.x_height / 2 - dc.stroke / 2
+        s = dc.stroke
+        delta = (s * (h * sqrt(w**2 + h**2 - s**2) - s * w)) / (h**2 - s**2)
+        theta = atan2(h, w - delta)
+        # delta = s / cos(theta)
 
-        # Angle of each branch
-        a = width - neck_len
-        b = fc.x_height / 2
-        theta = atan(b / a)
-        delta = stroke / sin(theta)
-        x_neck = x1 + neck_len + stroke - delta / 2
-
-
-        # Left ascender line
-        draw_rect(pen, x1, 0, x1 + stroke, fc.ascent)
-        # Middle junction
+        # Left ascender stem
+        draw_rect(pen, b.x1, 0, b.x1 + dc.stroke, dc.ascent)
+        # Middle junction connecting stem to branches
         draw_rect(
             pen,
-            x1 + stroke,
-            fc.x_height / 2 - stroke / 2,
+            b.x1 + dc.stroke,
+            b.ymid - dc.stroke / 2,
             x_neck,
-            fc.x_height / 2 + stroke / 2,
+            b.ymid + dc.stroke / 2,
         )
-        # Upper branch
+        # Upper branch (stem to top-right)
         draw_polygon(
             pen,
             points=[
-                (x_neck, fc.x_height / 2 + stroke / 2),
-                (x2 - delta / 2, fc.x_height),
-                (x2 + delta / 2, fc.x_height),
-                (x_neck + delta, fc.x_height / 2 + stroke / 2),
+                (x_neck, dc.x_height / 2 + dc.stroke / 2),
+                (b.x2 - delta, dc.x_height),
+                (b.x2, dc.x_height),
+                (x_neck + delta, dc.x_height / 2 + dc.stroke / 2),
                 (
-                    x_neck + delta - stroke * cos(theta),
-                    fc.x_height / 2 + stroke / 2 - stroke * sin(theta),
+                    x_neck + delta - dc.stroke * cos(theta),
+                    dc.x_height / 2 + dc.stroke / 2 - dc.stroke * sin(theta),
                 ),
-                (x_neck, fc.x_height / 2 - stroke / 2),
+                (x_neck, dc.x_height / 2 - dc.stroke / 2),
             ],
         )
-        # Lower branch
+        # Lower branch (stem to bottom-right)
         draw_polygon(
             pen,
             points=[
-                (x_neck, fc.x_height / 2 + stroke / 2),
+                (x_neck, dc.x_height / 2 + dc.stroke / 2),
                 (
-                    x_neck + delta - stroke * cos(theta),
-                    fc.x_height / 2 - stroke / 2 + stroke * sin(theta),
+                    x_neck + delta - dc.stroke * cos(theta),
+                    dc.x_height / 2 - dc.stroke / 2 + dc.stroke * sin(theta),
                 ),
-                (x_neck + delta, fc.x_height / 2 - stroke / 2),
-                (x2 + delta / 2, 0),
-                (x2 - delta / 2, 0),
-                (x_neck, fc.x_height / 2 - stroke / 2),
+                (x_neck + delta, dc.x_height / 2 - dc.stroke / 2),
+                (b.x2, 0),
+                (b.x2 - delta, 0),
+                (x_neck, dc.x_height / 2 - dc.stroke / 2),
             ],
         )
