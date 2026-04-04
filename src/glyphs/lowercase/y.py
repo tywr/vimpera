103098812cda2cc226e1361f8f79763e24885823
tywr <tanguy.viv@gmail.com>
2026-04-04 12:33:36 +0200

    refactor: y

--- src/glyphs/lowercase/y.py
+++ src/glyphs/lowercase/y.py
@@ -1,6 +1,7 @@
-from math import atan, cos, sin
+from math import atan, cos, sin, tan
 from glyph import Glyph
 from shapes.polygon import draw_polygon
+from shapes.parallelogramm import draw_parallelogramm
 
 
 class LowercaseYGlyph(Glyph):
@@ -8,36 +9,68 @@ class LowercaseYGlyph(Glyph):
     unicode = "0x79"
     offset = 0
     width_ratio = 380 / 340
+    overlap = 0.05
+    dent_ratio = 0.1
 
     def draw(self, pen, dc):
         b = dc.body_bounds(offset=self.offset, width_ratio=self.width_ratio)
-        half_width = b.width / 2 - dc.stroke / 2
+        dent_height = self.dent_ratio * b.height
 
-        branch_h = dc.x_height / 2
-        theta = atan(branch_h / half_width)
-        x_delta = dc.stroke / sin(theta)
-        y_delta = dc.stroke / cos(theta)
-
-        # Horizontal projection of the tail and dent offsets
-        x_delta_desc = -dc.descent * half_width / dc.x_height
-        x_delta_dent = dc.dent * half_width / dc.x_height
-
-        draw_polygon(
+        draw_parallelogramm(
             pen,
-            points=[
-                # Tail at descender
-                (b.xmid - x_delta / 2 - x_delta_desc, dc.descent),
-                (b.xmid + x_delta / 2 - x_delta_desc, dc.descent),
-                # Top-right branch
-                (b.xmid + half_width + x_delta / 2, dc.x_height),
-                (b.xmid + half_width - x_delta / 2, dc.x_height),
-                # Inner junction
-                (b.xmid, y_delta),
-                # Top-left branch
-                (b.xmid - half_width + x_delta / 2, dc.x_height),
-                (b.xmid - half_width - x_delta / 2, dc.x_height),
-                # Dent on left stem
-                (b.xmid - x_delta / 2 - x_delta_dent, dc.dent),
-                (b.xmid - x_delta / 2 + x_delta_dent, dc.dent),
-            ],
+            dc.stroke,
+            b.xmid + self.overlap * b.width,
+            dent_height,
+            b.x1,
+            b.y2,
+            direction="top-left",
         )
+        theta, delta = draw_parallelogramm(
+            pen,
+            dc.stroke,
+            b.xmid - self.overlap * b.width,
+            dent_height,
+            b.x2,
+            b.y2,
+        )
+        draw_parallelogramm(
+            pen,
+            dc.stroke,
+            b.xmid - self.overlap * b.width + delta,
+            dent_height,
+            b.xmid
+            - self.overlap * b.width
+            - (abs(dc.descent) + dent_height) / tan(theta),
+            dc.descent,
+            direction="bottom-left",
+        )
+        # half_width = b.width / 2 - dc.stroke / 2
+        #
+        # branch_h = dc.x_height / 2
+        # theta = atan(branch_h / half_width)
+        # x_delta = dc.stroke / sin(theta)
+        # y_delta = dc.stroke / cos(theta)
+        #
+        # # Horizontal projection of the tail and dent offsets
+        # x_delta_desc = -dc.descent * half_width / dc.x_height
+        # x_delta_dent = dc.dent * half_width / dc.x_height
+        #
+        # draw_polygon(
+        #     pen,
+        #     points=[
+        #         # Tail at descender
+        #         (b.xmid - x_delta / 2 - x_delta_desc, dc.descent),
+        #         (b.xmid + x_delta / 2 - x_delta_desc, dc.descent),
+        #         # Top-right branch
+        #         (b.xmid + half_width + x_delta / 2, dc.x_height),
+        #         (b.xmid + half_width - x_delta / 2, dc.x_height),
+        #         # Inner junction
+        #         (b.xmid, y_delta),
+        #         # Top-left branch
+        #         (b.xmid - half_width + x_delta / 2, dc.x_height),
+        #         (b.xmid - half_width - x_delta / 2, dc.x_height),
+        #         # Dent on left stem
+        #         (b.xmid - x_delta / 2 - x_delta_dent, dc.dent),
+        #         (b.xmid - x_delta / 2 + x_delta_dent, dc.dent),
+        #     ],
+        # )
--- src/shapes/parallelogramm.py
+++ src/shapes/parallelogramm.py
@@ -27,5 +27,10 @@ def draw_parallelogramm(
             pen, points=[(x1, y1), (x2 + delta, y2), (x2, y2), (x1 - delta, y1)]
         )
         return theta, delta
+    elif direction == "bottom-left":
+        draw_polygon(
+            pen, points=[(x1, y1), (x1 - delta, y1), (x2, y2), (x2 + delta, y2)]
+        )
+        return theta, delta
     else:
         raise ValueError("Value should be either `top` or `bottom`")
